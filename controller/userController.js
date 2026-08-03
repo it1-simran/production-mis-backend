e7e78994beb4aa8b6cb4389747a92d0ab2dcfa4b
Gurpreet <it2@jsdelectronics.co.in>
2026-08-03 13:10:36 +0530

    Device status and production date into the carton

--- controller/userController.js
+++ controller/userController.js
@@ -478,7 +478,7 @@ module.exports = {
       
       const rawPassword = String(req?.body?.password || "").trim();
       if (rawPassword) {
-        const bcrypt = require("bcryptjs");
+        const bcrypt = require("bcrypt");
         const salt = await bcrypt.genSalt(10);
         updatedData.password = await bcrypt.hash(rawPassword, salt);
       }
